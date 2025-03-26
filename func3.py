6ac9788041bdf7abcfef4a0d36f2b3c16b594126
harik654253 <harik654253@gmail.com>
2025-03-26 07:52:27 +0530

    Add files via upload

--- func3.py
+++ func3.py
@@ -13,3 +13,11 @@ def f2(a1,b1):
 
 f2(1,10)
 
+def f3(a1 = 7,b1 = 8,c1 = 10): 
+    
+    sum = a1+b1+c1
+    print("the sum of {},{},{} is".format(a1,b1,c1),sum)
+
+f3()
+f3(2,10)
+f3(b1=50)
--- func_keyargs.py
+++ func_keyargs.py
@@ -8,3 +8,9 @@ f1(name='hari',place='rjy') #function calling
 
 
 
+def f2(first_name,last_name):
+    
+    print("first name",first_name)
+    print("last name",last_name)
+
+f2(first_name = 'hari',last_name = 'krishna')
